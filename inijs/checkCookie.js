54ad092e7f8822d7fbbbc66e8523dfa6420bf8d6
Maulana Imanulhaq <imanulhaq5@gmail.com>
2023-12-01 21:46:02 +0700

    push return

--- inijs/checkCookie.js
+++ inijs/checkCookie.js
@@ -1,9 +1,9 @@
 import { getCookie } from "./cookies.js";
 
 export default function checkCookie() {
-    let username = getCookie("username");
-	if (username != "") {
-	alert("Hi " + username +" Welcome to Fancypedia");
+    let token = getCookie("username");
+	if (token != "") {
+	alert("Hi Welcome to Fancypedia");
 	} else {
 		alert("Biasakan Login Dulu Ya Bang!")
 
