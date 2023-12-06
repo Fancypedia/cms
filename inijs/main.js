95153ab8874d90c9927ad64f9be48288872ca4e6
Maulana Imanulhaq <imanulhaq5@gmail.com>
2023-12-06 10:20:23 +0700

    push

---
+++ inijs/main.js
@@ -0,0 +1,44 @@
+// import RegistrasiUser from "./signup.js";
+// // import { getCookie } from "./cookies.js";
+
+// window.RegistrasiUser = RegistrasiUser;
+
+// // const myHeaders = new Headers();
+// // let tokencookie = getCookie("token");
+// // myHeaders.set("token", tokencookie);
+
+
+
+import editProduct from "../scriptupdated2.js";
+import editComment from "../scriptupdatedcomment2.js";
+
+
+
+import createBlog from "./content/createblog.js";
+import createKomen from "./content/createcomment.js";
+import buatprodukkk from "./content/createproduct.js";
+
+// import { getCookie } from "./cookies.js";
+
+window.createBlog = createBlog;
+window.createKomen = createKomen;
+window.buatprodukkk = buatprodukkk;
+
+
+window.editProduct = editProduct;
+window.editComment = editComment;
+
+// window.updateProduct = updateProduct(event);
+
+
+
+
+import Logout from "./logout.js";
+import checkCookie from "./checkCookie.js";
+
+window.Logout = Logout;
+window.checkCookie = checkCookie;
+// const myHeaders = new Headers();
+// let tokencookie = getCookie("token");
+// myHeaders.set("token", tokencookie);
+
